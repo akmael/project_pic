2e5e0f8f58dfe536dd3e1aa3b7d8792b9af8d290
AndraIshmael <ishmaelak@gmail.com>
2016-03-18 16:45:07 -0400

    basic angular structure

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+angular.module('picApp', [
+
+]);
+
+angular.module('picApp')
+       .contorller('picController', picController);
+
+picController.$inject = ['$scope', '$http'];
+
+
+function picController($scope, $http){
+
+// search button functionaility
+$scope.searchTags = function(){
+
+}
+
+
+
+}
