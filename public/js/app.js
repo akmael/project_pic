d8df9499b3cab7fd1efabf56e576cb199840cc48
AndraIshmael <ishmaelak@gmail.com>
2016-03-28 21:40:11 -0400

    cleaned up some  of the testing code"

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 
-angular.module('picApp', ['angular-loading-bar', 'ngAnimate']);
+angular.module('picApp', ['angular-loading-bar', 'ngAnimate','ui.bootstrap']);
 
 
 angular.module('picApp')
@@ -20,7 +20,7 @@ $scope.searchTags = function(){
        .then(function(response){
          console.log("searching for photos now ");
          $scope.photos = response.data.photos.photo;
-         console.log($scope.photos)
+
 
        })
        .catch(function (err){
@@ -31,24 +31,14 @@ $scope.searchTags = function(){
 }
 //request for search by click of image
 $scope.cameraInfo = function(photo){
-  var farm = photo.farm,
-     server= photo.server,
-     id = photo.id,
-     secret= photo.secret
-     title = photo.title;
 
-  bootbox.dialog({
-    message: "<img class='imgsize pull-right' ng-src='http://farm"+farm+".static.flickr.com/"+server+"/"+id+"_"+secret+"_l.jpg' alt='"+title+"'>",
-    title: photo.title,
-    size: 'large'
-  });
-  $http.post('api/photos/camera/'+photo.id)
-        .then(function(response){
-          console.log("retriving info");
-          $scope.info = response.data.photo.camera;
-          console.log(response);
-        });
+ $http.post('api/photos/camera/'+photo.id)
+           .then(function(response){
+             console.log("retriving info");
+             $scope.info = response.data.photo.camera;
 
+
+           });
 
 }
 
